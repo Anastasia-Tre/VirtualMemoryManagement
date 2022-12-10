aeb64fa5ddfb61dd582b23b4896d8975e525b391
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-12-10 19:33:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VirtualMemoryManagment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
